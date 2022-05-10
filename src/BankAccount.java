f0eba6ecb89304250012d0135768e05fbc63c5e1
cmanor <101116379+cmanor@users.noreply.github.com>
2022-05-10 16:14:03 -0400

    working, may try to add scanner

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -3,19 +3,30 @@ public class BankAccount {
     private String name;
 
     public BankAccount(float balance, String name){
-        this.balance = balance;
-        this.name = name;
+        this.setBalance(balance);
+        this.setName(name);
     }
 
     public void deposit(float amount){
-        this.balance += amount;
+        this.setBalance(this.balance + amount);
     }
 
     public void withdrawal(float amount){
-        this.balance -= amount;
+        this.setBalance(this.balance - amount);
     }
 
     public void details(){
         System.out.println(name + "'s account balance is: " + balance);
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public void setName(String name) {
+        if (name.equals(""))
+            throw new IllegalArgumentException("Please enter a name");
+        this.name = name;
+
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -1,12 +1,14 @@
+import org.jetbrains.annotations.NotNull;
+
 public class Product {
     private float cost;
     private int quantity;
     private String productName;
 
     public Product(float cost, int quantity, String productName){
-        this.cost = cost;
-        this.quantity = quantity;
-        this.productName = productName;
+        this.setCost(cost);
+        this.setQuantity(quantity);
+        this.setProductName(productName);
     }
     public void totalCost(){
         float bill = quantity * cost;
@@ -16,5 +18,23 @@ public class Product {
     public void printProduct(){
         System.out.println("1 " + productName + " costs " + cost
         + " and " + quantity + " units were purchased");
+    }
+
+    public void setCost(float cost) {
+        if (cost <0)
+            throw new IllegalArgumentException("Cost must be equal or greater to 0");
+        this.cost = cost;
+    }
+
+    public void setQuantity(int quantity) {
+        if (quantity <0)
+            throw new IllegalArgumentException("Quantity must be equal or greater to 0");
+        this.quantity = quantity;
+    }
+
+    public void setProductName(@NotNull String productName) {
+        if (productName.equals(""))
+            throw new IllegalArgumentException("Product must have a name");
+        this.productName = productName;
     }
 }
